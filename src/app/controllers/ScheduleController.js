69a01be83650e022563e3087781416142e92dfc1
gabriel <gdcardoso@gmail.com>
2019-07-19 10:11:27 -0300

    configuring mongodb using mongoose

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,35 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class ScheculeController {
+  async index(req, res) {
+    const checkUserPovider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserPovider) {
+      return res.json({ error: 'User is not a provider' });
+    }
+
+    const { date } = req.query;
+    const parseDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheculeController();
