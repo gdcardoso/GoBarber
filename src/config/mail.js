f990731c14846764c20449975ef0714bcc96d160
gabriel <gdcardoso@gmail.com>
2019-07-22 11:39:03 -0300

    Cancellation of scheduling. Notification through Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '5d073aae9b2e30',
+    pass: 'bf8bda437696a5',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
