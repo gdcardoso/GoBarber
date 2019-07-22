2529aec337d5bb7dd3e0b87c8a180e7dd11f87ec
gabriel <gdcardoso@gmail.com>
2019-07-22 19:38:31 -0300

    listing available appointments

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddlware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
@@ -23,6 +24,7 @@ routes.use(authMiddlware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
